5fb4d6f055df1253a5888529b21633691a94a31e
Shivani Bhakta <sdpatel0612@gmail.com>
2024-01-28 11:55:00 +0100

    db.js changes

--- DB.js
+++ DB.js
@@ -1,16 +1,28 @@
 const express = require("express");
 const app = express();
 const port = 3000;
-const mysql = require("mysql2");
+const mysql = require("mysql");
+const path = require("path");
+
 const connection = mysql.createConnection({
-  host: "127.0.0.1",
+  server: "sql11.freemysqlhosting.net",
   port: 3306,
-  user: "root",
-  password: "root",
-  database: "AddressBook",
+  user: "sql11680282",
+  password: "xnlqsVnlfG",
+  database: "sql11680282",
 });
+
+connection.connect(function (err) {
+  if (err) {
+    console.error("Error connecting to the database: " + err.stack);
+    return;
+  }
+
+  console.log("Connected to the database as id " + connection.threadId);
+});
+
 app.use(express.json());
-const path = require("node:path");
+
 app.get("/", function (req, res) {
   res.sendFile(path.join(__dirname, "index.html"));
 });
